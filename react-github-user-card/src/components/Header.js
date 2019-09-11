db979bfad1a6c035d1bb5cc01ae59e5005a64587
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-11 11:52:42 -0700

    commit

---
+++ react-github-user-card/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+    constructor() {
+        super ()
+    };
+
+    render () {
+           
+        return (
+
+            <header className="App-header">
+                
+                <h1>React GitHub User Card</h1>
+
+            </header>
+
+        )
+        
+     }
+};
+
+export default Header;
